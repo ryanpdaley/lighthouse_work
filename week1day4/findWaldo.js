916fd05bcd9d33ea9e3fff71b8fbc3d447d18ad5
Ryan Daley <rpdaley@gmail.com>
2017-03-09 19:09:22 +0000

    fixing week1day4 git

---
+++ week1day4/findWaldo.js
@@ -0,0 +1,22 @@
+// The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+function actionWhenFound(index) {
+  console.log(`Found Waldo at index ${index}!`);
+}
+
+function findWaldo(arr, found){
+  arr.forEach(function(element, index) {
+    if (element === "Waldo") {
+      found(index);
+    }
+  });
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
